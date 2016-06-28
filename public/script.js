9f9debdb5e609f81205cf69a5b30030fbfa3b729
Elias <elias0789@gmail.com>
2016-06-28 22:40:57 +0100

    commit current state, wip

--- public/script.js
+++ public/script.js
@@ -1,9 +1,172 @@
-var Authenticate = new Authenticate()
+var rooms = {};
+var media = {};
+var accepted = {};
+var TAG = 'IPC-VIDEO-CLIENT:';
+var host = 'https://fac1.ipcortex.net';
+IPCortex.PBX.Auth.setHost(host);
 
-$('#login').on('click', function () {
-  console.log($('#login').html())
-  if ($('#login').html() === 'logout')
-    Authenticate.logout()
-  else
-    Authenticate.login()
-})
+function processFeed(av) {
+  /* Only process the Av instance if it has remote media */
+  if (typeof (av.remoteMedia) != 'object')
+    return;
+  var videos = [];
+  var feed = document.getElementById(av.id);
+  for (var id in av.remoteMedia) {
+    var video = document.getElementById(id);
+    if (av.remoteMedia[id].status === 'offered') {
+      /* If the remote party if offering create an invite */
+      if (accepted[av.id])
+        return;
+      console.log(TAG, 'Offer recieved from ' + av.remoteMedia[id].cN);
+      /* Mark the offer as accepted as we may get another
+         update with the 'offer' state still set */
+      accepted[av.id] = true;
+
+      navigator.mediaDevices.getUserMedia({ video: false, audio: true })
+        .then((stream) => {
+          console.log(TAG, 'Accepting offer with user stream');
+          av.accept(stream);
+        });
+    } else if (av.remoteMedia[id].status === 'connected' && !video) {
+      console.log(TAG, 'New remote media source ' + av.remoteMedia[id]);
+      /* Create a new video tag to play/display the remote media */
+      video = document.createElement('video');
+      attachMediaStream(video, av.remoteMedia[id]);
+      videos.push(video);
+      video.id = id;
+      video.play();
+    } else if (av.remoteMedia[id].status !== 'connected' && video) {
+      /* Remove any video tags that are no longer in a 'connected' state */
+      video.parentNode.removeChild(video);
+    }
+  }
+  /* Create a feed container to hold video tags if it doesn't exist */
+  if (videos.length && !feed) {
+    feed = document.createElement('div');
+    document.body.appendChild(feed);
+    feed.className = 'feed';
+    feed.id = av.id;
+  }
+  /* Add the new video tags to the feed container */
+  videos.forEach(function (_video) {
+    feed.appendChild(_video);
+  });
+  /* Remove the feed container if empty */
+  if (feed && feed.children.length < 1)
+    feed.parentNode.removeChild(feed);
+}
+
+function processContact(contact) {
+  /* Don't process contacts that match the logged in user */
+  if (contact.cID === IPCortex.PBX.Auth.id)
+    return;
+  var element = document.getElementById(contact.cID);
+  /* Return early if contact exists */
+  if (element) {
+    /* Remove offline contacts */
+    if (!contact.canChat && element.parentNode)
+      element.parentNode.removeChild(element);
+    return;
+  }
+  /* Create online contact */
+  if (contact.canChat) {
+    element = document.createElement('div');
+    document.getElementById('contacts').appendChild(element);
+    element.innerHTML = contact.name;
+    element.className = 'contact';
+    element.id = contact.cID;
+    var offer = document.createElement('i');
+    element.appendChild(offer);
+    offer.className = 'material-icons contact-offer';
+    offer.innerHTML = 'video_call';
+    offer.addEventListener('click', function () {
+      contact.chat();
+    });
+    offer.addEventListener('click',
+      function () {
+        navigator.mediaDevices.getUserMedia({ audio: true, video: true }).then(
+          /* Grab the user media */
+          function (stream) {
+            /* Check to see if a room already exists to start video on */
+            for (var roomID in rooms) {
+              if (rooms[roomID].cID !== contact.cID)
+                continue;
+              /* Listen for updates on the Av instance */
+              rooms[roomID].videoChat(stream).addListener('update', processFeed);
+              return;
+            }
+            /* No room to start video, store stream and contact ID and open a new room */
+            media = { cID: contact.cID, stream: stream };
+            contact.chat();
+          }
+        ).catch(
+          function () {
+            console.log(TAG, 'getUserMedia failed');
+          }
+        );
+      }
+    );
+  }
+}
+
+function startTask() {
+  /* Start API collecting data */
+  IPCortex.PBX.startFeed().then(
+    function () {
+      console.log(TAG, 'Live data feed started');
+      /* API is ready, loop through the list of contacts */
+      IPCortex.PBX.contacts.forEach(
+        function (contact) {
+          /* Listen for updates incase the contact changes state */
+          contact.addListener('update', processContact);
+          processContact(contact);
+        }
+      );
+      /* Enable chat to allow feature (video negotiation) messages to be exchanged */
+      var chatEnabled = IPCortex.PBX.enableChat(function (room) {
+        /* Listen for updates to clean up dead rooms */
+        room.addListener('update', function (_room) {
+          if (rooms[_room.roomID] && _room.state === 'dead')
+            delete rooms[_room.roomID];
+        });
+        /* If the room has come into existance due to a video request,
+           start video with the stored stream */
+        if (room.cID === media.cID && media.stream) {
+          console.log(TAG, 'New room, starting video chat');
+          /* Listen for updates on the Av instance */
+          room.videoChat(media.stream).addListener('update', processFeed);
+          media = {};
+        }
+        rooms[room.roomID] = room;
+      }
+      );
+      if (chatEnabled) {
+        console.log(TAG, 'Chat enable, enabling av feature');
+        /* Register to receive new Av instances */
+        IPCortex.PBX.enableFeature(
+          'av',
+          function (av) {
+            /* Listen for updates to the Av instance */
+            av.addListener('update', processFeed);
+            processFeed(av);
+          },
+          ['chat']
+        );
+      }
+    },
+    function () {
+      console.log(TAG, 'Live data feed failed');
+    }
+  );
+}
+
+/* Login */
+IPCortex.PBX.Auth.login().then(
+  function () {
+    console.log(TAG, 'Login successful');
+    startTask();
+  },
+  function () {
+    console.log(TAG, 'Login failed');
+  }
+);
